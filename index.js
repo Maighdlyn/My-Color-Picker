f58d113697bf859359e202a5698936cd1ed7f0ed
Emma <maighdlyn@Branwen.local>
2017-02-24 22:43:54 -0800

    First Color Picker
    Each button has it’s own click function
    
    Only three buttons for red, blue, and yellow background options

---
+++ index.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+  $('.red').click(function(){
+    $('body').removeClass();
+    $('body').addClass('red');
+  });
+
+  $('.blue').click(function(){
+    $('body').removeClass();
+    $('body').addClass('blue');
+  });
+
+  $('.yellow').click(function(){
+    $('body').removeClass();
+    $('body').addClass('yellow');
+  });
+
+});
